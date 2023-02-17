ad09445ab19a1198fb992757c4ae919c61972ef5
Quinten Robley <qprtechman@gmail.com>
2023-02-17 01:18:05 -0600

    completion of automatic radar data acquisition

--- radar_data.py
+++ radar_data.py
@@ -1,3 +1,4 @@
+import os
 import dateutil
 import pandas as pd
 import tornado_data as td
@@ -11,20 +12,21 @@ tornado_df = None
 stations_df = None
 radar_bucket = None
 surface_radius = 6371000.0
+S3_BUCKET_NAME = "noaa-nexrad-level2"
 
 def load_data():
 	global tornado_df
 	global stations_df
 	global radar_bucket
+	global radar_object
 	stations_df = pd.read_csv("./data/Radar/NEXRAD_Stations.csv")
 	stations_df = stations_df.loc[stations_df.STATION_ID.str.contains("NEXRAD:T")==False]
 	tornado_df = td.get_all_tornados()
 	print("All hail the rat god!")
 	
-	S3_BUCKET_NAME = "noaa-nexrad-level2"
-	s3_resource = boto3.resource("s3")
-	s3_resource.meta.client.meta.events.register('choose-signer.s3.*', disable_signing)
-	radar_bucket = s3_resource.Bucket(S3_BUCKET_NAME)
+	s3object = boto3.resource("s3")
+	s3object.meta.client.meta.events.register('choose-signer.s3.*', disable_signing)
+	radar_bucket = s3object.Bucket(S3_BUCKET_NAME)
 	# print('Listing Amazon S3 Bucket objects/files:')
 	# for obj in radar_bucket.objects.filter(Prefix='2018'):
 	#     print(f'-- {obj.key}')
@@ -41,15 +43,49 @@ def find_distance(lat1, lon1, lat2, lon2):
 	return surface_radius * c
 	
 def download_file_check(obj, date, station_name, begin_unix_time, end_unix_time):
-	date_sections = str(obj.key).replace((date + '/' + station_name + '/' + station_name), '').split('_')
+	date_sections = obj.replace((date + '/' + station_name + '/' + station_name), '').split('_')
 	d = date_sections[0][0:4] + "-" + date_sections[0][4:6] + "-" + date_sections[0][6:]
 	t = date_sections[1][0:2] + ":" + date_sections[1][2:4] + ":" + date_sections[1][4:]
 	unix_time = dateutil.parser.parse((d + ' ' + t + ' +0')).timestamp()
 	if unix_time > begin_unix_time and unix_time < end_unix_time:
-		print("File Time: " + str(dt.datetime.utcfromtimestamp(unix_time)))
+		# print("File Time: " + str(dt.datetime.utcfromtimestamp(unix_time)))
 		return True
 	else:
 		return False
+
+def find_l2_files(prefix):
+	path = "./data/Radar/s3cache/" + prefix.replace('/', '-')
+	objs = []
+	if os.path.isfile(path):
+		file = open(path, "r")
+		objs = file.read().split('\n')
+		file.close()
+		if objs[-1] == '':
+			objs = objs[:-1]
+	else:
+		bucket_data = radar_bucket.objects.filter(Prefix=prefix)
+		file = open(path, "w")
+		objs = list(map(lambda x: x.key, bucket_data))
+		file.write("\n".join(objs))
+		file.close()
+	return objs
+
+def download_l2_files(files_to_download):
+	global radar_bucket
+	for obj in files_to_download:
+		if obj.split('_')[-1] != 'MDM':
+			file_name = obj.split('/')[-1]
+			file_path = "./data/Radar/temp/" + file_name
+			new_file_path = "./data/Radar/l2data/" + file_name
+			if not os.path.isfile(new_file_path):
+				radar_bucket.download_file(obj, file_path)
+				print("Download of " + file_name + " completed")
+				os.rename(file_path, new_file_path)
+				print("Moving of " + file_name + " from './data/Radar/temp/' to './data/Radar/l2data/' completed")
+			else:
+				print("File " + file_name + " already is downloaded")
+		else:
+			print("We don't know what MDM files do, so we will ignore them")
 
 def download_nearest_operational_radar_data(tornado, stations_to_remove = []):
 	global stations_df
@@ -59,7 +95,7 @@ def download_nearest_operational_radar_data(tornado, stations_to_remove = []):
 	avg_lon = (tornado["BEGIN_LON"] + tornado["END_LON"])/2
 
 	temp_stations_df = stations_df
-
+	# completion of automatic radar data acquisition
 	# remove stations that were down during the time (based on previous runs of the function)
 	if stations_to_remove != []:
 		for station in stations_to_remove:
@@ -71,6 +107,7 @@ def download_nearest_operational_radar_data(tornado, stations_to_remove = []):
 		distances.append(find_distance(avg_lat, avg_lon, station[1]['LATITUDE'], station[1]['LONGITUDE']))
 
 	temp_stations_df['DISTANCE'] = distances
+	temp_stations_df = temp_stations_df.loc[temp_stations_df['DISTANCE'] < 400000]
 	ans = temp_stations_df.iloc[(temp_stations_df['DISTANCE']).abs().argsort()[:1]]
 	print("Station Found: ")
 	print(ans)
@@ -86,36 +123,47 @@ def download_nearest_operational_radar_data(tornado, stations_to_remove = []):
 	station_name_index = station_id.find(":") + 1
 	station_name = station_id[station_name_index:(station_name_index + 4)]
 
-	prefix = begin_date + '/' + station_name + '/' + station_name + begin_date.replace('/', '') + '_'
-	alt_prefix = end_date + '/' + station_name + '/' + station_name + end_date.replace('/', '') + '_'
+	prefix = begin_date + '/' + station_name + '/' + station_name + begin_date.replace('/', '')
+	alt_prefix = end_date + '/' + station_name + '/' + station_name + end_date.replace('/', '')
 
-	file_to_download = []
-	print("Begin Time: " + str(begin_date_time))
-	for obj in radar_bucket.objects.filter(Prefix=prefix):
-		if download_file_check(obj, begin_date, station_name, dateutil.parser.parse(begin_date_time.strftime("%Y-%m-%d %H:%M:%S") + ' +0').timestamp(), tornado['END_TIME_UNIX']):
-			file_to_download.append(obj.key)
+	files_to_download = []
+	# print("Begin Time: " + str(begin_date_time))
+	for obj in find_l2_files(prefix):
+		if download_file_check(obj, begin_date, station_name, dateutil.parser.parse(begin_date_time.strftime("%Y-%m-%d %H:%M:%S") + ' +0').timestamp(), dateutil.parser.parse(end_date_time.strftime("%Y-%m-%d %H:%M:%S") + ' +0').timestamp()):
+			files_to_download.append(obj)
 	if begin_date != end_date:
-		for obj in radar_bucket.objects.filter(Prefix=alt_prefix):
-			if download_file_check(obj, end_date, station_name, dateutil.parser.parse(begin_date_time.strftime("%Y-%m-%d %H:%M:%S") + ' +0').timestamp(), tornado['END_TIME_UNIX']):
-				file_to_download.append(obj.key)
-	print("End Time: " + str(end_date_time))
-	print("Files to Download: ")
-	print(file_to_download)
+		for obj in find_l2_files(alt_prefix):
+			if download_file_check(obj, end_date, station_name, dateutil.parser.parse(begin_date_time.strftime("%Y-%m-%d %H:%M:%S") + ' +0').timestamp(), dateutil.parser.parse(end_date_time.strftime("%Y-%m-%d %H:%M:%S") + ' +0').timestamp()):
+				files_to_download.append(obj)
+	# print("End Time: " + str(end_date_time))
 
-	if file_to_download == []:
+	print("Files to Download: ")
+	print(files_to_download)
+
+	if files_to_download == []:
 		stations_to_remove.append("NEXRAD:" + station_name)
 		download_nearest_operational_radar_data(tornado, stations_to_remove)
 
-	
+	download_l2_files(files_to_download)
 
-	
-def download_radar_data():
+def get_radar_data():
 	global tornado_df
 	load_data()
+	try:
+		os.mkdir('./data/Radar/l2data')
+	except OSError as err:
+		pass
+	try:
+		os.mkdir('./data/Radar/s3cache')
+	except OSError as err:
+		pass
+	try:
+		os.mkdir('./data/Radar/temp')
+	except OSError as err:
+		pass
 	for tornado in tornado_df.iterrows():
 		download_nearest_operational_radar_data(tornado[1])
 		break
 		
 
-
-download_radar_data()
+get_radar_data()
